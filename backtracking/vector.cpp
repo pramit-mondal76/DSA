078c46d8a52248a0131627b1b260f26e7ffc4e0c
ᎮᏒᏗᎷᎥᏖ ᎷᎧᏁᎴᏗᏝ <95114633+pramit-mondal@users.noreply.github.com>
2022-05-28 10:25:59 +0530

    Add files via upload

---
+++ backtracking/vector.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printArray(vector<int> arr,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void fillArray(vector<int> &arr,int i,int n,int val)
+{
+    if(i==n){
+        printArray(arr,n);
+        return;
+    }
+    arr[i]=val;
+    fillArray(arr,i+1,n,val+1);
+    arr[i]=-1*arr[i]; 
+}
+int main()
+{
+    vector<int>arr(100,0);
+    int n;
+    cout<<"Enter size of the array: ";
+    cin>>n;
+    fillArray(arr,0,n,1);
+    printArray(arr,n);
+    return 0;
+}
